a92eb92de257bb557af76cf829c55c78643380fb
Roger Mendez <rdmendez7571@users.noreply.github.com>
2019-05-02 07:02:29 -0400

    Break statement description

--- MendezIntegration/src/Task.java
+++ MendezIntegration/src/Task.java
@@ -5,29 +5,31 @@ import java.util.Scanner;
 public class Task {
 
   public void simpleMessage(String name) {
-    
+
     Scanner input = new Scanner(System.in);
-    
+
 
     // print out greeting using escape methods
     System.out.println("Hey! How are you doing " + name + "? "
         + "\nWelcome to my integration project! I hope you're doing well and that you enjoy looking "
         + "through my program! ");
-    
+
     System.out.println("Please enter a command here:");
-    String text =input.nextLine();
-    
+    String text = input.nextLine();
+
     switch (text) {
       case "start":
-      System.out.println("Program has booted up!");
-      break;
-      
+        System.out.println("Program has booted up!");
+        break;
+      // break statements are necessary because without them, statements in switch blocks fall
+      // through; each break statement terminates the enclosing switch statement
+
       case "stop":
         System.out.println("Program is running slowly.");
         break;
-        
-        default:
-          System.out.println("ERROR! Command not recognized. Please try again.");
+
+      default:
+        System.out.println("ERROR! Command not recognized. Please try again.");
     }
   }
 }
